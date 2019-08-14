f653ee08270d53b96dbb4fff144f677dc03d3400
dsanderson90 <dsanderson3@cnm.edu>
2019-08-14 13:00:19 -0600

    products can now be created, read, updated, and deleted.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb+srv://admin:abcd1234@cluster0-myg00.mongodb.net/test?retryWrites=true&w=majority'
+let dev_db_url = 'mongodb+srv://admin:abcd1234@cluster0-myg00.mongodb.net/products?retryWrites=true&w=majority'
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -21,5 +21,5 @@ app.use('/products', product);
 let port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -14,4 +14,28 @@ exports.product_create = function (req, res) {
       }
       res.send('Product Created successfully')
   })
+
+};
+
+exports.product_details = function (req, res) {
+  Product.findById(req.params.id, function (err, product) {
+      if (err) return next(err);
+      res.send(product);
+  })
+};
+
+exports.product_update = (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, {$set: req.body},
+    function (err, product) {
+      if(err) return next(err);
+      res.send('Product Updated.')
+    });
+};
+
+exports.product_delete = (req, res) => {
+  Product.findByIdAndRemove(req.params.id,
+    function (err) {
+      if(err) return next(err);
+      res.send('Deleted Successfully!')
+    });
 };
