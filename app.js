47f088fd9f048a162ca6b571c3a78145651eab30
dsanderson90 <dsanderson3@cnm.edu>
2019-08-20 17:31:39 -0600

    organized app.js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 const product = require('./routes/product.route'); // Imports routes for the products
-const app = express();
+app.use('/products', product);
+
 
 // Set up mongoose connection
-const mongoose = require('mongoose');
 let dev_db_url = ''
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/products', product);
+
 
 let port = process.env.PORT || 3000;
 
